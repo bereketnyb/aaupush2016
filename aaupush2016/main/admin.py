cf412a92e92c741d9ca2b8f02dd92172385f0f6f
genioaddis <genioaddis@gmail.com>
2016-08-01 17:34:01 +0300

    Register models in admin.py

--- aaupush2016/main/admin.py
+++ aaupush2016/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+models = [Section, Lecturer, Material, Announcement, Course, Department]
+
+for model in models:
+	admin.site.register(model)
